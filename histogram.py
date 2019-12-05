235be69cde8ccacc488b1ded20a3643792337462
Thibault Thomas <thibault.thomas@epitech.eu>
2019-12-05 02:11:42 +0100

    Add comments change files architecture

--- histogram.py
+++ histogram.py
@@ -33,12 +33,12 @@ def showBrowsers(data: list) -> None:
     showPlot(data, "Browser", "Occurrences", "Number of occurrences per browser")
 
 
-def showPlot(data: list, xLabel: str, yLabel: str, title: str) -> None:
+def showPlot(data: list, x_label: str, y_label: str, title: str) -> None:
     """
 
     :param data: The list of data to represent
-    :param xLabel: The title to set on the x Axis
-    :param yLabel: The title to set on the y Axis
+    :param x_label: The title to set on the x Axis
+    :param y_label: The title to set on the y Axis
     :param title: The window's title
     """
     values = {}
@@ -54,8 +54,8 @@ def showPlot(data: list, xLabel: str, yLabel: str, title: str) -> None:
     energy = values.values()
     x_pos = [i for i, _ in enumerate(x)]
     plt.bar(x_pos, energy, color='green')
-    plt.xlabel(xLabel)
-    plt.ylabel(yLabel)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.title(title)
 
     plt.xticks(x_pos, x)
